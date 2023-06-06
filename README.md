4a3ca30c7907b871da1c78092ee7d64a5922bc5d
Monico <monicobellon233@gmail.com>
2023-06-06 08:16:14 +0200

    cambios de iva

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Mi primer repositorio de GitHub
 
 
+CAMBIOS DEL IVA
--- main.c
+++ main.c
@@ -2,6 +2,10 @@ int main(void)
 {
     printf ("Hola mundo que me estas viendo");
     printf ("¿Como estas?");
-    
+    prueba(133);
+}
+
+int prueba (int l){
+    printf "Esto es un numero de iva %d ",l);
 }
 
